e33657219f8bc295e0d5d006a42efd0e48379785
Anay Khandekar <ayankr0504@gmail.com>
2024-01-30 14:37:47 -0600

    new base

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useState } from 'react'
+import Typist from 'react-typist'
+
+const App: React.FC = () => {
+  return (
+    <Typist>
+      Hello, world!
+    </Typist>
+  );
+}
+
+export default App;
