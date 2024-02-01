eadcded5c2dff259c854407675e436f929a9f238
anaykhandekar <ayankr0504@gmail.com>
2024-02-01 16:45:56 -0600

    typing completed

--- src/components/TypingPhase.tsx
+++ src/components/TypingPhase.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { dialogueAudioMap } from '../global/dialogue'; // Import the dialogueAudioMap
 
 interface TypingPhaseProps {
   onComplete: () => void;
@@ -8,14 +9,73 @@ const styles = {
   typingText: {
     color: 'white',
     fontSize: '32px',
-    fontFamily: 'Courier New'
+    fontFamily: 'Courier New',
+    whiteSpace: 'pre-wrap' // To maintain whitespace and line breaks in the text
+  },
+  startButton: {
+    padding: '10px 20px',
+    fontSize: '20px',
+    cursor: 'pointer',
+    margin: '20px',
+    backgroundColor: '#f0f0f0'
   }
 }
 
+const phrases = [
+  "Hi my love ♥",
+  "I made this website to ask you something.",
+  "But you have to answer some things first.",
+  "Flip over your letter. Good luck baby.",
+  "muah muah muuaaah muah muah",
+  "Find each word. Use its highest column number. Solve the puzzle."
+];
+
 const TypingPhase: React.FC<TypingPhaseProps> = ({ onComplete }) => {
+  const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
+  const [typedText, setTypedText] = useState('');
+  const [hasStarted, setHasStarted] = useState(false);
+
+  useEffect(() => {
+    if (hasStarted) {
+      // Play the corresponding audio when a new phrase starts typing
+      if (typedText === '' && currentPhraseIndex < phrases.length) {
+        const audio = new Audio(dialogueAudioMap[currentPhraseIndex]);
+        audio.play();
+      }
+
+      if (currentPhraseIndex < phrases.length) {
+        const currentPhrase = phrases[currentPhraseIndex];
+        if (typedText !== currentPhrase) {
+          // This is where you adjust the typing speed
+          const timeoutId = setTimeout(() => {
+            setTypedText(currentPhrase.slice(0, typedText.length + 1));
+          }, 60); // Typing speed adjusted to be faster
+          return () => clearTimeout(timeoutId);
+        } else {
+          const timeoutId = setTimeout(() => {
+            setCurrentPhraseIndex(currentPhraseIndex + 1);
+            setTypedText(''); // Reset for the next phrase
+          }, 2000); // Adjust delay before typing next phrase
+          return () => clearTimeout(timeoutId);
+        }
+      } else {
+        onComplete(); // All phrases typed, call onComplete
+      }
+    }
+  }, [typedText, currentPhraseIndex, onComplete, hasStarted]);
+
   return (
-    <div style={styles.typingText}>
-        <button onClick={onComplete}>Complete Typing</button>
+    <div>
+      {!hasStarted && (
+        <button style={styles.startButton} onClick={() => setHasStarted(true)}>
+          Start
+        </button>
+      )}
+      {hasStarted && (
+        <div style={styles.typingText}>
+          {typedText}
+        </div>
+      )}
     </div>
   );
 }
